f185a33f99eff366865891ea97b6e8b8434275ff
Nitesh Nandan <nitesh.na@imimobile.com>
2020-12-02 21:02:46 +0530

    initial project add closes

---
+++ src/main/java/nitesh/springframework/niteshbeerservice/NiteshBeerServiceApplication.java
@@ -0,0 +1,13 @@
+package nitesh.springframework.niteshbeerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NiteshBeerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NiteshBeerServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nitesh/springframework/niteshbeerservice/NiteshBeerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package nitesh.springframework.niteshbeerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NiteshBeerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
